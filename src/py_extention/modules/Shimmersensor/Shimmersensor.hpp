47e25ed38b68a7499efb52486476905e1d6b8851
Ayberk <s210697@th-ab.de>
2023-05-03 14:05:19 +0200

    Shimmer-Sensor Modul erstellt

---
+++ src/py_extention/modules/Shimmersensor/Shimmersensor.hpp
@@ -0,0 +1,72 @@
+#include "../../PyRunner.hpp"
+#include "../../websocket/SocketClient.hpp"
+
+#include "../../../BSlogger.hpp"
+
+#include "proto/ShimmersensorProt.pb.h"
+
+#include <string>
+
+class Shimmersensor {
+    private:
+        PythonRunner* runner;
+        SocketClient client;
+        int moduleIndex;
+    public:
+        Shimmersensor(PythonRunner* runner):client("127.0.0.1", 50008){
+            this->runner = runner;
+            this->moduleIndex = runner->LoadModule("Shimmersensor");
+        }
+        ~Shimmersensor(){
+            if (runner->getFunctionRunning(moduleIndex)){
+                runner->JoinProcess(moduleIndex);
+            }
+            runner->UnloadModule(moduleIndex);
+        }
+
+        void StartServer(){
+            LOG_INIT_CERR();
+            log(LOG_INFO) << "Starting server\n";
+            runner->StartProcess(moduleIndex, "startServer", {});
+            runner->JoinProcess(moduleIndex);
+
+            log(LOG_INFO) << "Waiting for connection\n";
+            runner->StartProcess(moduleIndex, "serverWaitConnection", {});
+            int state = client.sockConnect();
+            log(LOG_INFO) << "Connection state: " << state << "\n";
+            runner->JoinProcess(moduleIndex);
+
+        }
+
+        void StartSensor(){
+            runner->StartProcess(moduleIndex,"StarteSensor", {});
+            runner->JoinProcess(moduleIndex);
+        }
+
+        void fetchData(){
+            LOG_INIT_CERR();
+            log(LOG_INFO) << "Getting data\n";
+            std::string data;
+            client.sockRecv(data);
+            shimmer::DataPacket pb;
+            pb.ParseFromString(data);
+            log(LOG_INFO) << "Data: " << pb.accel_ln_x() << "\n";
+
+        }
+
+        void StoppeSensor(){
+            runner->StartProcess(moduleIndex,"StoppeSensor", {});
+            runner->JoinProcess(moduleIndex);
+        }
+
+
+
+        void StopServer(){
+            LOG_INIT_CERR();
+            log(LOG_INFO) << "Stopping server\n";
+            client.sockDisconnect();
+            runner->StartProcess(moduleIndex, "stopServer", {});
+            runner->JoinProcess(moduleIndex);
+        }
+};
+
--- src/py_extention/modules/Shimmersensor/Shimmersensor.py
+++ src/py_extention/modules/Shimmersensor/Shimmersensor.py
@@ -1,37 +1,60 @@
 import time
-import Shimmersensor_pb2
+import scripts.Shimmersensorptot_pb2 as proto
 
 from serial import Serial
 
 from pyshimmer import ShimmerBluetooth, DEFAULT_BAUDRATE, DataPacket, EChannelType
 
+
+
 class Shimmersensor: 
-    def handler(pkt: DataPacket) -> None:
-    cur_value = pkt[EChannelType.ACCEL_LN_X]
-    print(cur_value)
+    shim_dev = None
+    sock = None
+    conn = None
+    addr = None
+    pb = None
 
-    def starteSensor():
-    serial = Serial('COM5', DEFAULT_BAUDRATE)
-    shim_dev = ShimmerBluetooth(serial)
+    def Handler(pkt: DataPacket) -> None:
+        cur_value = pkt[EChannelType.ACCEL_LN_X]
+        print(cur_value)
+        pb.accel_ln_x = cur_value
+        self.conn.sendall(pb.SerializeToString())
+        print('Test data sent.')
 
-    shim_dev.initialize()
+    def StarteSensor():
+        serial = Serial('COM5', DEFAULT_BAUDRATE)
+        shim_dev = ShimmerBluetooth(serial)
 
-    dev_name = shim_dev.get_device_name()
-    print(f'My name is: {dev_name}')
+        shim_dev.initialize()
 
-    shim_dev.add_stream_callback(handler)
+        dev_name = shim_dev.get_device_name()
+        print(f'My name is: {dev_name}')
 
-    shim_dev.start_streaming()
-    
-    
-if __name__ == '__main__':
-    
-    starteSensor()
-   
-    shim_dev.stop_streaming()
+        shim_dev.add_stream_callback(handler)
 
-    shim_dev.shutdown()
-    input()
+        shim_dev.start_streaming()
+
+
+    def StoppeSensor():
+        shim_dev.stop_streaming()
+        shim_dev.shutdown()
+
+    def startServer(self):
+        print("Starting server...")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((HOST, PORT))
+        self.sock.listen(1)
+        self.pb = proto.DataPacket()
+        print("Server started.")
+
+    def serverWaitConnection(self):
+        print ('Waiting for connection...')
+        self.conn, self.addr = self.sock.accept()
+
+    def stopServer(self):
+        self.conn.close()
+        print ('Server closed.')
+
 
 class ShimmerData:
     def init(self, accel_x=0.0):
