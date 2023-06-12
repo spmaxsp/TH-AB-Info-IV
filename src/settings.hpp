cb76b8f701ce50ac75b36327cf604f2f22163390
Maximilian Spahn <dev@max-spahn.de>
2023-06-12 19:22:41 +0200

    fixing settings

--- src/settings.hpp
+++ src/settings.hpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <string>
+#include <iomanip>
+
 #include <json.hpp>
 
 #define DIFFICULTY_EASY 0
@@ -66,12 +68,25 @@ public:
         file.close();
 
         try {
-            shimmer = settingsData.at("shimmer").get<ShimmerSettings>();
-            eeg = settingsData.at("eeg").get<EegSettings>();
-            head = settingsData.at("head").get<HeadSettings>();
-            main = settingsData.at("main").get<MainSettings>();
-            python = settingsData.at("python").get<PythonSettings>();
-            score = settingsData.at("score").get<ScoreSettings>();
+            shimmer.com = settingsData["shimmer"]["com"];
+            shimmer.pollrate = settingsData["shimmer"]["pollrate"];
+
+            eeg.user_id = settingsData["eeg"]["user_id"];
+            eeg.token = settingsData["eeg"]["token"];
+            eeg.pollrate = settingsData["eeg"]["pollrate"];
+
+            head.step_size = settingsData["head"]["step_size"];
+
+            main.difficulty = settingsData["main"]["difficulty"];
+            main.sensor = settingsData["main"]["sensor"];
+            main.time = settingsData["main"]["time"];
+            main.virtual_head = settingsData["main"]["virtual_head"];
+
+            python.path = settingsData["python"]["path"];
+            python.clean_exit_flag = settingsData["python"]["clean_exit_flag"];
+
+            score.high_score = settingsData["score"]["high_score"];
+
         } catch (const json::exception&) {
             // Error occurred while parsing the settings, handle it accordingly
             return false;
@@ -81,12 +96,24 @@ public:
     }
 
     bool saveSettings() {
-        settingsData["shimmer"] = shimmer;
-        settingsData["eeg"] = eeg;
-        settingsData["head"] = head;
-        settingsData["main"] = main;
-        settingsData["python"] = python;
-        settingsData["score"] = score;
+        settingsData["shimmer"]["com"] = shimmer.com;
+        settingsData["shimmer"]["pollrate"] = shimmer.pollrate;
+
+        settingsData["eeg"]["user_id"] = eeg.user_id;
+        settingsData["eeg"]["token"] = eeg.token;
+        settingsData["eeg"]["pollrate"] = eeg.pollrate;
+
+        settingsData["head"]["step_size"] = head.step_size;
+
+        settingsData["main"]["difficulty"] = main.difficulty;
+        settingsData["main"]["sensor"] = main.sensor;
+        settingsData["main"]["time"] = main.time;
+        settingsData["main"]["virtual_head"] = main.virtual_head;
+
+        settingsData["python"]["path"] = python.path;
+        settingsData["python"]["clean_exit_flag"] = python.clean_exit_flag;
+
+        settingsData["score"]["high_score"] = score.high_score;
 
         std::ofstream file(fileName);
         if (!file.is_open()) {
@@ -111,8 +138,8 @@ private:
 
         head.step_size = 0.1;
 
-        main.difficulty = "easy";
-        main.sensor = "accelerometer";
+        main.difficulty = DIFFICULTY_EASY;
+        main.sensor = SENSOR_SHIMMER;
         main.time = 60;
         main.virtual_head = false;
 
